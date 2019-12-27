277bf4267df10bcd93d35d76b413f66dae7e1f9e
Kamrul Hasan <kamrul.ruetcse@gmail.com>
2019-12-27 14:48:30 +0600

    Footer copyright.

--- functions.php
+++ functions.php
@@ -30,3 +30,18 @@ function change_adv_position( $shoppingcart_default_values ) {
 }
 
  add_filter( 'shoppingcart_get_option_defaults_values', 'change_adv_position' );
+
+ // Footer Copyright
+ add_action('widgets_init', 'afraeshop_widgets_init', 1000);
+function afraeshop_widgets_init() {
+
+	register_sidebar(array(
+			'name' => __('Copyright Sidebar', 'afraeshop'),
+			'id' => 'shoppingcart_footer_options',
+			'description' => __('Shows widgets for copyright.', 'afraeshop'),
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget' => '</aside>',
+			'before_title' => '<h2 class="widget-title">',
+			'after_title' => '</h2>',
+		));
+}
